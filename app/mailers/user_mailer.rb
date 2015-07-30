dcc5049d2740015f8b0663e217f0c9e720aa33fe
Stephen Ribbon <stribbon@icloud.com>
2015-07-30 16:26:47 -0700

    env email var

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def user_added(user, sphere)
     @user = user
     @sphere = sphere
-    @url = 'http://localhost:3000'
+    @url = ENV['URL']
     mail(to: "<#{user.email}>", subject: "h0mie$: You have been added to a Sphere")
   end
 end
