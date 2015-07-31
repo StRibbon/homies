9f18cf1c313f227e84525733533cefaeb96a8060
Stephen Ribbon <stribbon@icloud.com>
2015-07-30 17:02:48 -0700

    changing rails secrets to ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => Rails.application.secrets[:MANDRILL_USERNAME], # SMTP password is any valid API key
-    :password  => Rails.application.secrets[:MANDRILL_PASSWORD], # SMTP password is any valid API key
+    :user_name => ENV[:MANDRILL_USERNAME], # SMTP password is any valid API key
+    :password  => ENV[:MANDRILL_PASSWORD], # SMTP password is any valid API key
     :domain => 'heroku.com',
     :authenication => "plain"
   }
