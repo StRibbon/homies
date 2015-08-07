0cd27eae469965ee21947f094289a77b59ffd7c6
Stephen Ribbon <stribbon@icloud.com>
2015-08-07 12:56:48 -0700

    only invited users can sign-up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,8 @@ class StaticPagesController < ApplicationController
   end
 
   def email_invite
-  	InviteMailer.invite_user(current_user, params[:email]).deliver_now
+  	@invite = Invite.create(email: params[:email])
+  	InviteMailer.invite_user(current_user, params[:email]).deliver_now 	
   	redirect_to user_spheres_path(user_id: session[:user_id]), flash: {success: "#{params[:email]} has been invited!"}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :ensure_correct_user, only: [:show,:edit, :update, :destroy]
+  # before_action :check_invite, only: [:create]
 	
   def index
   	@users = User.all
@@ -11,13 +12,22 @@ class UsersController < ApplicationController
   def show
   end
   def create
-    @user = User.new user_params
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_spheres_path(session[:user_id]), flash: {success: "Created!"}
-    else
-      render :new
+    user_email = params['user']['email']
+    invite = Invite.find_by_email(user_email)
+    if(invite)
+      invite_email = invite.email
     end
+      if(user_email == invite_email)
+        @user = User.new user_params
+          if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_spheres_path(session[:user_id]), flash: {success: "Created!"}
+          else
+            render :new
+          end
+      else 
+        redirect_to root_path, flash: {alert: "Sorry you have not been invited yet"}
+      end
   end
   private
 
@@ -35,9 +45,10 @@ class UsersController < ApplicationController
     end
 
     def ensure_correct_user
-    # compare some params vs something in the session/current_user
-    unless params[:id].to_i == session[:user_id]
-      redirect_to user_spheres_path, alert: "Not Authorized"
+      # compare some params vs something in the session/current_user
+        unless params[:id].to_i == session[:user_id]
+          redirect_to user_spheres_path, alert: "Not Authorized"
+      end
     end
-  end
+
 end
